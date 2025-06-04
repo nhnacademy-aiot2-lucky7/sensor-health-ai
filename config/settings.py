b8107b1360655f82c166a70d6f25a3020c4eecba
jy37414 <pjy37414@gmail.com>
2025-06-04 16:53:28 +0900

    분석시간 동적으로 기입할 수 있게 구현(default: 01:30, 02:30)

--- config/settings.py
+++ config/settings.py
@@ -11,3 +11,6 @@ ANALYSIS_RESULT_API_URL = os.getenv("ANALYSIS_RESULT_API_URL")
 # 예측 기준 시간
 PREDICTION_CUTOFF_HOUR = 2  # 새벽 2시 기준
 PREDICTION_LOG_FILE = "prediction_log.txt"
+
+JOB_MAIN_TIME = os.getenv("JOB_MAIN_TIME", "02:30")
+JOB_CHECK_TIME = os.getenv("JOB_CHECK_TIME", "01:30")
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from scheduler.analyze_schedule import job_main, job_check
-import schedule
 import time
+import schedule
+from config.settings import JOB_MAIN_TIME, JOB_CHECK_TIME
+from scheduler.analyze_schedule import job_main, job_check
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-schedule.every().day.at("01:30").do(job_check)
-schedule.every().day.at("02:30").do(job_main)
+schedule.every().day.at(JOB_MAIN_TIME).do(job_main)
+schedule.every().day.at(JOB_CHECK_TIME).do(job_check)
 
 if __name__ == "__main__":
     logger.info("스케줄러 시작")
