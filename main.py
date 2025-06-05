668a07524d074b8c5acf64dfc78772c53823bccf
jy37414 <pjy37414@gmail.com>
2025-06-05 09:36:21 +0900

    로그 추가

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ import logging
 import schedule
 from config.settings import JOB_MAIN_TIME, JOB_CHECK_TIME
 from scheduler.analyze_schedule import job_main, job_check
+import signal
+import sys
 
 logger = logging.getLogger(__name__)
 
 schedule.every().day.at(JOB_MAIN_TIME).do(job_main)
 schedule.every().day.at(JOB_CHECK_TIME).do(job_check)
 
+# 종료 신호 처리 함수
+def signal_handler(sig, frame):
+    logger.info("스케줄러 종료")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
 if __name__ == "__main__":
     logger.info("스케줄러 시작")
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+    try:
+        while True:
+            logger.debug("작업 대기 중...")
+            schedule.run_pending()
+            time.sleep(60)
+    except Exception as e:
+        logger.exception(f"스케줄러 실행 중 예외 발생: {e}")
+        logger.info("스케줄러 종료")
