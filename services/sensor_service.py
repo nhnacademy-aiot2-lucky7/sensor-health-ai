7040be7e66be9e1e7356a04afbc82ac225f2e8c4
jy37414 <pjy37414@gmail.com>
2025-06-07 15:54:47 +0900

    sensor url에 맞게 수정

--- services/sensor_service.py
+++ services/sensor_service.py
@@ -7,7 +7,7 @@ from config.settings import SENSOR_API_URL
 import logging
 
 DATA_DIR = "data"
-SENSOR_ENDPOINT = f"{SENSOR_API_URL}/api/threshold-histories"
+SENSOR_ENDPOINT = f"{SENSOR_API_URL}/api/threshold-histories/date"
 logger = logging.getLogger(__name__)
 
 def flatten_sensor_data(raw_data: dict) -> pd.DataFrame:
