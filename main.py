546221d0df5bd938e84a85b01fd149a6740f6db4
jy37414 <pjy37414@gmail.com>
2025-06-07 17:05:45 +0900

    예측 시간 변경해서 테스트

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import sys
 
 logger = logging.getLogger(__name__)
 
-schedule.every().day.at(JOB_MAIN_TIME).do(job_main)
-schedule.every().day.at(JOB_CHECK_TIME).do(job_check)
+schedule.every().day.at("08:08").do(job_main)
+schedule.every().day.at("08:13").do(job_check)
 
 # 종료 신호 처리 함수
 def signal_handler(sig, frame):
